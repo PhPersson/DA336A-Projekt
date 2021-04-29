a0bd1f5aca82bcbc3ceaba61a411a5520a3c705c
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2021-04-29 11:36:25 +0200

    Ny databas

--- src/model/Hash.java
+++ src/model/Hash.java
@@ -1,2 +1,26 @@
-package model;public class Hash {
-}
+package model;
+
+import org.springframework.security.crypto.bcrypt.*;
+
+public class Hash {
+
+
+
+    public static String hashPass(String password){
+        return BCrypt.hashpw(password,BCrypt.gensalt());
+    }
+
+    public static boolean checkHash(String password, String hash) {
+        boolean password_verified = false;
+
+        if(null == hash || !hash.startsWith("$2a$"))
+            throw new java.lang.IllegalArgumentException("Invalid hash provided for comparison");
+
+        password_verified = BCrypt.checkpw(password, hash);
+
+        return(password_verified);
+    }
+    }
+
+
+
