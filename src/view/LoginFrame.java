a3147ba71378c2fd11b34c54afcc6a956594e7a8
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2021-05-19 11:20:30 +0200

    Fixade bugg att loginFrame stängs ner

--- src/view/LoginFrame.java
+++ src/view/LoginFrame.java
@@ -152,7 +152,6 @@ public class LoginFrame extends JFrame implements ActionListener{
             registerFrame = new RegisterFrame(controller);
         } else if (e.getSource() == btnLogin) {
           controller.btnLoginClicked();
-          this.dispose();
         } else if (e.getSource() == btnNoLogin) {
             controller.btnNoLoginClicked();
             this.dispose();
@@ -168,7 +167,6 @@ public class LoginFrame extends JFrame implements ActionListener{
  */
 class LoginButtonController implements DocumentListener{
     private JButton login;
-    private LoginFrame frame;
 
     LoginButtonController(JButton b){
         login = b;
